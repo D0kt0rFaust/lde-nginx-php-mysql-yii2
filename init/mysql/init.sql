b96bc7caf4862962ea7241ab6265b2993234b590
michael <michael@bolt-system.ru>
2023-10-03 22:37:05 +0300

    v0.3 .gitignore fix

---
+++ init/mysql/init.sql
@@ -0,0 +1,33 @@
+-- CREATE DATABASE IF NOT EXISTS `local`;
+-- USE `local`;
+-- CREATE USER 'local-user'@'%' IDENTIFIED WITH mysql_native_password BY 'local-password';
+-- GRANT ALL PRIVILEGES ON *.* TO 'local-user'@'%';
+-- FLUSH PRIVILEGES;
+-- CREATE TABLE `migration_bootstrap` (
+--   `version` varchar(180) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL,
+--   `apply_time` int DEFAULT NULL
+-- ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --
+-- -- Дамп данных таблицы `migration_bootstrap`
+-- --
+
+-- INSERT INTO `migration_bootstrap` (`version`, `apply_time`) VALUES
+-- ('m000000_000000_base', 1675961994),
+-- ('m170523_164923_translite', 1675961994),
+-- ('m170523_164934_category', 1675961996),
+-- ('m170523_164935_confirm_hash', 1675961996),
+-- ('m170523_164937_dictionary', 1675961997),
+-- ('m170523_164938_dictionary_linked', 1675962018),
+-- ('m170523_164939_error_log', 1675962020),
+-- ('m170523_164942_event_log', 1675962021),
+-- ('m170523_164946_file', 1675962023),
+-- ('m170523_164954_language', 1675962024),
+-- ('m170523_164955_log', 1675962024),
+-- ('m170523_164956_mail', 1675962025),
+-- ('m170523_165007_push_notifications', 1675962025),
+-- ('m170523_165011_system_language', 1675962025),
+-- ('m170526_041331_create_imports_table', 1675962026),
+-- ('m170920_170735_create_table_gallery', 1675962026),
+-- ('m170927_145308_create_table_modules', 1675962027),
+-- ('m171130_211320_create_page', 1675962027);
